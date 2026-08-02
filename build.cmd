38dc912dfbb0676fc7ee76b5f65f103a928c842e
HanSiqi2008 <136245260+HanSiqi2008@users.noreply.github.com>
2026-08-02 14:53:51 +0800

    Delete build.cmd
    开始清理一些不必要的东西

--- build.cmd
+++
@@ -1,5 +0,0 @@
-powershell -Command "Invoke-WebRequest -Uri 'https://api.xiaofengqwq.com/StaticResources/Mscpo/servers.dat' -OutFile '.\pkgbuild\servers.dat'"
-cd pkgbuild
-"..\packwiz.exe" mr export
-echo "Finish! You can find the modpack at ./pkgbuild"
-pause
